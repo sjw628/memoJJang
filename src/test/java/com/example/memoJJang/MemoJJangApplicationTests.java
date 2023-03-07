2a9e6a2b2337f65f3aa569dd961c668403f487fa
sjw628 <105299357+sjw628@users.noreply.github.com>
2023-03-08 01:01:41 +0900

    project upload
    project upload

---
+++ src/test/java/com/example/memoJJang/MemoJJangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoJJang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoJJangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
